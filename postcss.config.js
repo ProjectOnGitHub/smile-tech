dbe93c1b7423b517146096ae15a23edcb481f3d2
Aleksandr <soft@vhlv.ru>
2023-03-23 19:54:16 +0300

    refactor: change display questions info

--- postcss.config.js
+++ postcss.config.js
@@ -2,10 +2,5 @@ const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 module.exports = {
-  plugins: [
-    autoprefixer({
-      browsers: ['ie >= 8', 'last 20 version'],
-    }),
-    cssnano({ preset: 'default' }),
-  ],
+  plugins: [autoprefixer({}), cssnano({ preset: 'default' })],
 };
