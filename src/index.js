16c356ae323d3bde9c0556cecdb2821c3790ed1e
Aleksandr <soft@vhlv.ru>
2023-03-23 14:36:04 +0300

    feat: add show/hide answers

--- src/index.js
+++ src/index.js
@@ -5,3 +5,17 @@ function requireAll(icon) {
 }
 
 requireAll(require.context('../src/svg', true, /\.svg$/));
+
+const list = document.querySelector('.questions-info__list');
+function toggleQuestions(e) {
+  const { parentElement } = e.target;
+  const text = parentElement.querySelector('.questions-info__text');
+  const button = parentElement.querySelector('.questions-info__button');
+  const title = e.target.classList.contains('questions-info__title');
+  if (title || button) {
+    text.classList.toggle('questions-info__text_visible');
+    button.classList.toggle('questions-info__button_rotate');
+  }
+}
+
+list.addEventListener('click', toggleQuestions);
